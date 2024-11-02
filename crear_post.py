9b7d3c1d06ece33dabf5c7cd1b00c40ad13eef2a
sergi <sergioquesada35@gmail.com>
2024-11-02 03:10:25 +0100

    ahora está todo unido

--- crear_post.py
+++ crear_post.py
@@ -1,32 +1,38 @@
 import requests
-import time
+from subir_archivo import upload_attachment
+
+url = "https://indev.comty.app/api/posts/new"
+headers = {
+    "Authorization":"Server 31aaa395-22f5-4528-9406-38dc0e05d21e:gQmpSd5dr8WS1hi7GsufY6lBceII5ZYK7b8Q"
+}
+
 salir = False
 while not salir:
-    access_id = "31aaa395-22f5-4528-9406-38dc0e05d21e"
-    secret_token = "gQmpSd5dr8WS1hi7GsufY6lBceII5ZYK7b8Q"
+    messageInput = input("¿Que deseas publicar?")
+    attachmentInput = input("¿Deseas subir un archivo?")
 
-    url = "https://indev.comty.app/api/posts/new"
+    if attachmentInput == "si":
+        attachmentResult = upload_attachment()
 
     data = {
-        "message": input("¿Que deseas publicar?"),
+        "message": messageInput,
         "attachments": [
-            {"url" : str(input("Introduzca link de la imagen: "))}
+            attachmentResult,
         ]
     }
-    headers = {
-        "Authorization":"Server 31aaa395-22f5-4528-9406-38dc0e05d21e:gQmpSd5dr8WS1hi7GsufY6lBceII5ZYK7b8Q"
-    }
+
     response = requests.post(url, json=data, headers=headers)
+
     if response.status_code == 200:
         print("Item creado:", response.json())
     else:
         print(f"Error: {response.status_code}")
 
     comprobacion = input("¿Desea seguir publicando?(si,no)")
+
     if comprobacion == "si":
         salir = False
     elif comprobacion == "no":
         salir = True
     else:
         salir = True
-
--- subir_archivo.py
+++ subir_archivo.py
@@ -2,42 +2,37 @@ import requests
 import tkinter as tk
 from tkinter import filedialog
 
-def seleccionar_archivo():
-    # Abrir el cuadro de diálogo para seleccionar un archivo
-    ruta_archivo = filedialog.askopenfilename(
+uploadFileUrl = "https://indev.comty.app/api/upload/file"
+headers = {
+    "Authorization":"Server 31aaa395-22f5-4528-9406-38dc0e05d21e:gQmpSd5dr8WS1hi7GsufY6lBceII5ZYK7b8Q",
+    "use-compression": "false",
+}
+
+def upload_attachment():
+    ventana = tk.Tk()
+    ventana.withdraw()
+
+    filepath = filedialog.askopenfilename(
         title="Seleccione un archivo",
         filetypes=(("Archivos de imagen", "*.jpg;*.png;*.gif;*.jpeg;*.mp4;"), ("Todos los archivos", "*.*"))
     )
 
-    if ruta_archivo:
-        print("Ruta seleccionada:", ruta_archivo)
-        return ruta_archivo
-    else:
+    if filepath is None:
         return None
 
-headers = {
-        "Authorization":"Server 31aaa395-22f5-4528-9406-38dc0e05d21e:gQmpSd5dr8WS1hi7GsufY6lBceII5ZYK7b8Q","use-compression": "false"}
-url = "https://indev.comty.app/api/upload/file"
-ventana = tk.Tk()
-ventana.withdraw()  # Ocultar la ventana principal de Tkinter
-# Abrimos el archivo en modo binario para la carga
-with open(seleccionar_archivo(), "rb") as file:
-    # Crear el diccionario para el campo 'file'
-    files = {
-        "file": file
-    }
+    with open(filepath, "rb") as file:
+        files = {
+            "file": file
+        }
 
-    # Realizar la petición POST
-    response = requests.post(url, files=files, headers=headers)
-    # Verificar si la petición fue exitosa (código 200)
-    if response.status_code == 200:
-        # Convertir la respuesta en JSON
-        data = response.json()
-        print("URL del archivo subido:", data["url"])
-        print("ID del archivo:", data["id"])
-    else:
-        print(f"Error al cargar el archivo: {response.status_code}")
-        print(response.text)
+        response = requests.post(uploadFileUrl, files=files, headers=headers)
 
+        if response.status_code == 200:
+            data = response.json()
 
+            print("URL del archivo subido:", data["url"])
+            print("ID del archivo:", data["id"])
 
+            return data
+        else:
+            return None
