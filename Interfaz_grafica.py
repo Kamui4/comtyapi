958a03e392bb44314047b0c6175c5a6b94941d66
sergi <sergioquesada35@gmail.com>
2024-11-09 19:55:03 +0100

    initial commit

--- Interfaz_grafica.py
+++ Interfaz_grafica.py
@@ -29,6 +29,7 @@ def crear_variable_entorno():
     print("Se va a cerrar el programa...")
     time.sleep(3)
     exit(0)
+
 def get_variable_entorno():
     if os.getenv("COMTY_API"):
         comty_api = os.getenv("COMTY_API")
@@ -36,17 +37,16 @@ def get_variable_entorno():
         return True
     elif "COMTY_API" in os.environ:
         comty_api = os.environ["COMTY_API"]
-        print(comty_api)
         return True
     else:
         print("No se encuentra el COMTY_API")
         return False
-uploadFileUrl = "https://indev.comty.app/api/upload/file"
+
 
 def upload_attachment():
     ventana = tk.Tk()
     ventana.withdraw()
-
+    uploadFileUrl = "https://indev.comty.app/api/upload/file"
     filepath = filedialog.askopenfilename(
         title="Seleccione un archivo",
         filetypes=(("Archivos de imagen y video", "*.jpg;*.png;*.gif;*.jpeg;*.mp4;"), ("Todos los archivos", "*.*"))
@@ -68,71 +68,59 @@ def upload_attachment():
             print(f"Error al subir el archivo: {response.status_code}")
             return None
 
-def activar_boton():
-    attachmentInput = attachment_entry.get().lower()  # Leer y normalizar el texto de attachment_entry
-    messageInput = message_entry.get()  # Leer el texto del mensaje
+def activar_boton(data):
+    url = "https://indev.comty.app/api/posts/new"
+    #attachmentInput = attachment_entry.get().lower()  # Leer y normalizar el texto de attachment_entry
+    messageInput = message_entry.get()
+    data['message'] = messageInput
+    try:
+        response = requests.post(url, json=data, headers=headers)
+        if response.status_code == 200:
+            print("Item creado:", response.json())
+            return
+        elif response.status_code == 500:
+            print("error de googlo")
+        else:
+            print(f"Error al crear el item: {response.status_code}","\nIbas a publicar: ",response.json())
+            return
+    except Exception as e:
+        print(f"Error al realizar la solicitud: {e}")
+        return
+
+def image_boton(data):
     # Verificar si se desea subir un archivo
-    if attachmentInput == "si" or estado_sino["boton_actual"] == "si":
+    if estado_upload["boton_actual"] == "si":
+        data['attachment'] = None
+        estado_upload["boton_actual"] = "no"
+        boton_upload.config(text=estado_upload["boton_actual"])
+        boton_upload.config(background="Red")
+    elif estado_upload["boton_actual"] == "no":
         attachmentResult = upload_attachment()
         if not attachmentResult:
-            errormsg = tk.Label(root, text="Error: No se ha podido subir el archivo.", font=("Arial", 14),fg="Red", background="#1e1f22")
+            errormsg = tk.Label(root, text="Error: No se ha podido subir el archivo.", font=("Arial", 14), fg="Red",background="#1e1f22")
             errormsg.pack()
             print("Error: No se ha podido subir el archivo.")
-            return
-        try:
-            data = {
-                "message": messageInput,
-                "attachments": [
-                attachmentResult
-                ]
-            }
-            response = requests.post(url, json=data, headers=headers)
-            if response.status_code == 200:
-                print("Item creado:", response.json())
-                return
-            else:
-                print(f"Error al crear el item: {response.status_code}")
-                return
-        except Exception as e:
-            print(f"Error al realizar la solicitud: {e}")
-            return
+        if 'id' in attachmentResult:
+            data['attachment'] = attachmentResult['id']
+        else:
+            print("Error: El archivo no contiene un ID.")
 
-    if attachmentInput == "no"or estado_sino["boton_actual"] == "no":
-        try:
-            data = {
-                "message": messageInput,
-                "attachments": [
-                None
-                ]
-            }
-            response = requests.post(url, json=data, headers=headers)
-            if response.status_code == 200:
-                print("Item creado:", response.json())
-                return
-            else:
-                print(f"Error al crear el item: {response.status_code}")
-                return
-        except Exception as e:
-            print(f"Error al realizar la solicitud: {e}")
-            return
-def activar_boton_sino(estado_sino):
-    if estado_sino["boton_actual"] == "si":
-        estado_sino["boton_actual"] = "no"
-        boton_sino.config(text=estado_sino["boton_actual"])
-        boton_sino.config(background="Red")
-    elif estado_sino["boton_actual"] == "no":
-        estado_sino["boton_actual"] = "si"
-        boton_sino.config(text=estado_sino["boton_actual"])
-        boton_sino.config(background="Green")
+        #data['attachment'] = attachmentResult
+        estado_upload["boton_actual"] = "si"
+        boton_upload.config(text=estado_upload["boton_actual"])
+        boton_upload.config(background="Green")
 
 # Configurar y verificar la variable de entorno
 get_variable_entorno()
-estado_sino = {"boton_actual": "no"}
+data = {'message':None, 'attachment':None}
+#Datos estados iniciales y datos
+attachmentResult = None
+estado_upload = {"boton_actual": "no"}
+uploadFileUrl = "https://indev.comty.app/api/upload/file"
+headers = {
+    'Authorization': 'Server ' + os.getenv("COMTY_API")
+}
 if get_variable_entorno() is True:
-    url = "https://indev.comty.app/api/posts/new"
-    headers = {
-        'Authorization': 'Server ' + os.getenv("COMTY_API")
-    }
     # Configuración de la interfaz gráfica
     root = tk.Tk()
     root.title("Comty API")
@@ -145,19 +133,11 @@ if get_variable_entorno() is True:
     message_entry = tk.Entry(root, font=("Arial", 14), background="#3f4148")
     message_entry.pack()
 
-    # POST archivo
-    foto_label = tk.Label(root, text="¿Deseas subir un archivo (si, no)", font=("Arial", 14), background="LightGray")
-    foto_label.pack()
-
-    #Sustituido por botón sino
-    #attachment_entry = tk.Entry(root, font=("Arial", 14),background="#3f4148")
-    #attachment_entry.pack()
-
-    # Botón sino
-    boton_sino = tk.Button(root, text=estado_sino["boton_actual"],background="Red", command=lambda:activar_boton_sino(estado_sino))
-    boton_sino.pack()
+    # Botón upload
+    boton_upload = tk.Button(root, text=estado_upload["boton_actual"], background="Red", command=lambda:image_boton(data))
+    boton_upload.pack()
     # Botón de activación
-    boton = tk.Button(root, text="Subir", command=activar_boton)
+    boton = tk.Button(root, text="Subir", command=lambda:activar_boton(data))
     boton.pack()
 
     # Botón de salir
